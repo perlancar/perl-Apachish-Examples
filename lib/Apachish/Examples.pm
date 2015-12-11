0da8be616003c362429c22142513557aa9b77a95
perlancar (@pc-office) <perlancar@gmail.com>
2015-12-11 17:03:34 +0700

    Initial commit

---
+++ lib/Apachish/Examples.pm
@@ -0,0 +1,19 @@
+package Apachish::Examples;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Example Apachish configuration files
+
+=head1 DESCRIPTION
+
+This distribution contains various examples of L<Apachish> configuration files
+in its per-dist share dir. It also serves as a spectest.
+
+
+=head1 SEE ALSO
+
+L<Apachish>
+
+L<Config::Apachish>, L<Config::Apachish::Reader>
